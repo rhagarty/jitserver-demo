a244de0fce9b51f6403b32cc8de48d691e62e6a2
Rich Hagarty <rich.hagarty@ibm.com>
2022-03-28 19:55:07 -0500

    update readme after review

--- AcmeAir/runJMeterJITServer.sh
+++ AcmeAir/runJMeterJITServer.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 # The last parameter in the line below is the IP of the machine where liberty runs
 # Output is written to  /output/acmeair.stats.0 inside the container
-docker run --rm --net=host  -e JTHREAD=2 -e JDURATION=120 -e JPORT=9093 -e JUSERBOTTOM=0  -e JUSER=198 --name jmeter-jitserver jmeter-acmeair:3.3 localhost
+docker run --rm --net=host  -e JTHREAD=2 -e JDURATION=120 -e JPORT=9093 -e JUSERBOTTOM=0  -e JUSER=99 --name jmeter-jitserver jmeter-acmeair:3.3 localhost
